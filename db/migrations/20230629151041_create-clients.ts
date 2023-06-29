1d29b815715bbef1abb12b6df4d96fe5b05d31f7
Diogo-gallina <diogogallina20@gmail.com>
2023-06-29 12:18:58 -0300

    feat: create-migrates-table-clients

---
+++ db/migrations/20230629151041_create-clients.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('clients', (table)=> {
+        table.uuid('id').primary()
+        table.text('name').notNullable()
+        table.text('cpf').notNullable
+        table.decimal('balance').notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('clients')
+}
+
