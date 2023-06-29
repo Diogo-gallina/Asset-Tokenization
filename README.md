a9e87761d68b09d2fc46d66efb107e31ad961f28
Diogo-gallina <diogogallina20@gmail.com>
2023-06-29 12:01:30 -0300

    feat: create-midration-table-tokens

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Asset-Tokenization
 🪙Asset token market simulation system
+
+
+RF
+
+- [ ] Deve ser possivel adicionar Tokens com valores e quantidades iniciais aleatórias
+
+- [ ] Tokens comprados devem ter o id do cliente que comprou
+
+- [ ] Cliente deve poder comprar um token
+
+- [ ] Cliente deve poder ver os tokens comprados, e a quantidade que foi comprada
---
+++ db/migrations/20230629144209_create-tokens.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('tokens', (table) => {
+        table.uuid('id').primary()
+        table.decimal('value').notNullable()
+        table.integer('quantity').notNullable()
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('tokens');
+}
+
